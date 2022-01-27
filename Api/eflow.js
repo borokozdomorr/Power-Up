07429aa3a62bafda7e1553e0066d2521c015e060
Burak <burak.ozdemir@netoloji.com>
2022-01-27 23:54:17 +0300

    test

--- Api/eflow.js
+++ Api/eflow.js
@@ -1,3 +1,19 @@
+window.Trello.authorize({
+    type: 'popup',
+    name: 'Getting Started Application',
+    scope: {
+      read: 'true',
+      write: 'true' },
+    expiration: 'never',
+    success: authenticationSuccess,
+    error: authenticationFailure
+  });
 
-
+  var authenticationSuccess = function() {
+    console.log('Successful authentication');
+  };
+  
+  var authenticationFailure = function() {
+    console.log('Failed authentication');
+  };
 console.log("Hello World");
