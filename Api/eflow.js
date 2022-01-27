bd79c784f45d687e7538edab0b76315e3f1ec252
Burak <burak.ozdemir@netoloji.com>
2022-01-28 00:17:51 +0300

    Update eflow.js

--- Api/eflow.js
+++ Api/eflow.js
@@ -1,16 +1,4 @@
-window.TrelloPowerUp.initialize({
-  'board-buttons': function (t, opts) {
-    return [{
-      icon: {
-        dark: WHITE_ICON,
-        light: BLACK_ICON
-      },
-      text: 'Callback',
-      callback: onBtnClick,
 
-    }
-  }
-});
 
 
 window.Trello.authorize({
@@ -32,21 +20,12 @@ window.Trello.authorize({
     console.log('Failed authentication');
   };
 
-  var myList = 'INSERT YOUR IDLIST HERE';
-
-  var creationSuccess = function (data) {
-    console.log('Card created successfully.');
-    console.log(JSON.stringify(data, null, 2));
-  };
-  
-  var newCard = {
-    name: 'aasdasdsadsa',
-    desc: 'This is the description of our new card.',
-    // Place this card at the top of our list
-    idList: myList,
-    pos: 'top'
-  };
-  
-  window.Trello.post('/cards/', newCard, creationSuccess);
+  window.TrelloPowerUp.initialize({
+    "board-buttons": function (t, opts) {
+      return t.board("all").then(function (board) {
+        console.log(JSON.stringify(board, null, 2));
+      });
+    },
+  });
 
 console.log("Hello World");
