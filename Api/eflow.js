46b73b8b28e2f8a69522539ab99255e990dcdbc0
Burak <burak.ozdemir@netoloji.com>
2022-01-30 18:32:20 +0300

    test

--- Api/eflow.js
+++ Api/eflow.js
@@ -5,36 +5,39 @@
 
 
   window.TrelloPowerUp.initialize({
-    'show-settings': function(t, options){
+   // 'show-settings': function(t, options){
       // when a user clicks the gear icon by your Power-Up in the Power-Ups menu
       // what should Trello show. We highly recommend the popup in this case as
       // it is the least disruptive, and fits in well with the rest of Trello's UX
-      return t.popup({
-        title: 'Custom Fields Settings',
-        url: './login.html',
-        height: 184 // we can always resize later
-      });
-    },
+     // return t.popup({
+     //   title: 'Custom Fields Settings',
+     ///   url: './login.html',
+     //   height: 184 // we can always resize later
+    //  });
+   // },
     'authorization-status': function(t, options){
+var Result = false;
 
-
-var Data = {
+var model = {
   "companyID":0,
   "username":"admin",
   "password":"admin"
 }
 var Url = "http://localhost:15055/Api/User/Login";
-
-jQuery.ajax({
+  
+ajax({
   url:Url,
   type:"POST",
-   data:Data
-})
+   data:JSON.stringify(model),
+   contentType: "application/json"
+}).done(function(res){
+Result = true;
+});
       // return a promise that resolves to the object with
       // a property 'authorized' being true/false
       // you can also return the object synchronously if you know
       // the answer synchronously
-      return new TrelloPowerUp.Promise((resolve) => resolve({ authorized: true }));
+      return new TrelloPowerUp.Promise((resolve) => resolve({ authorized: Result }));
     }
   });
 
