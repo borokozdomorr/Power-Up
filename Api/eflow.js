baa0e38765c238043a2177c21a02a8ccfa55d0bd
Burak <burak.ozdemir@netoloji.com>
2022-01-30 18:54:20 +0300

    ttsdsad

--- Api/eflow.js
+++ Api/eflow.js
@@ -4,5 +4,25 @@
 
 
 
-  
+    window.TrelloPowerUp.initialize({
+      'show-settings': function(t, options){
+         
+        return t.popup({
+        title: 'Custom Fields Settings',
+         url: './login.html',
+          height: 184 // we can always resize later
+       });
+      },
+     'show-authorization': function(t, options){
+      // return what to do when a user clicks the 'Authorize Account' link
+      // from the Power-Up gear icon which shows when 'authorization-status'
+      // returns { authorized: false }
+      // in this case we would open a popup
+      return t.popup({
+        title: 'E-Flow',
+        url: './login.html',
+        height: 500,
+      });
+    }
+    });
 
