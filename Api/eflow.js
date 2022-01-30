2854833fd85a46f1976c0c6e168346885843996f
Burak <burak.ozdemir@netoloji.com>
2022-01-30 18:57:30 +0300

    dsadsadsa

--- Api/eflow.js
+++ Api/eflow.js
@@ -13,6 +13,13 @@
           height: 184 // we can always resize later
        });
       },
+      'authorization-status': function(t, options){
+        // return a promise that resolves to the object with
+        // a property 'authorized' being true/false
+        // you can also return the object synchronously if you know
+        // the answer synchronously
+        return new TrelloPowerUp.Promise((resolve) => resolve({ authorized: true }));
+      },
      'show-authorization': function(t, options){
       // return what to do when a user clicks the 'Authorize Account' link
       // from the Power-Up gear icon which shows when 'authorization-status'
